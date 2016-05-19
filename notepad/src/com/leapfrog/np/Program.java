7a5400bb87a6e95e02330855ebb8a46107e6f327
beesal <kaalvairab@gmail.com>
2016-05-19 15:29:12 +0545

    First Commit

---
+++ notepad/src/com/leapfrog/np/Program.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leapfrog.np;
+
+import Notepad.Notepad1;
+
+/**
+ *
+ * @author Bishal
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new Notepad1().setVisible(true);
+    }
+    
+}
